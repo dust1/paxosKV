e8355a41e194a34b52301e2ae0e0788e752149c8
dust1 <834902408@qq.com>
2021-03-10 17:33:46 +0800

    Add 读取配置文件

--- paxosKV-server/src/main/java/com/dust/PaxosKVMainVerticle.java
+++ paxosKV-server/src/main/java/com/dust/PaxosKVMainVerticle.java
@@ -8,7 +8,11 @@ public class PaxosKVMainVerticle extends AbstractVerticle {
     
     @Override
     public void start() {
-        vertx.deployVerticle(KVServer.class.getName());
+        // vertx.deployVerticle(KVServer.class.getName());
+        // System.out.println("start...");
+        vertx.fileSystem().readFile("./conf/setting.json", res -> {
+            System.out.println(res.toString());
+        });
     }
 
 }
--- paxosKV-server/src/main/java/com/dust/kv/KVServer.java
+++ paxosKV-server/src/main/java/com/dust/kv/KVServer.java
@@ -1,5 +1,10 @@
 package com.dust.kv;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.commons.io.IOUtils;
+
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.net.NetServer;
 
@@ -9,7 +14,7 @@ public class KVServer extends AbstractVerticle {
     public void start() throws Exception {
         //TODO 这里读取配置并将信息传入网络服务器节点当中
         //TODO 这里应该从配置信息中读取到master节点，并从master节点获取到自身的哈希分布情况
-        
+        // IOUtils.
         NetServer server = vertx.createNetServer();
         server.listen(8080, "0.0.0.0", res -> {
             if (res.succeeded()) {
